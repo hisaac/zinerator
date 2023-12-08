4170b740448f35d65729c656d0024ae8f899978a
Isaac Halvorson <hello@hisaac.net>
2023-12-08 08:54:02 -0600

    Fix parsing of emojis until trafilatura / lxml is updated

--- notes.md
+++ notes.md
@@ -2,3 +2,7 @@
 
 Where I found some info: https://web.archive.org/web/20220610172120/http://blog.manbolo.com/2013/03/18/safari-reader-source-code
 The JavaScript source from that article: https://web.archive.org/web/20170313235147/http://blog.manbolo.com/2013/03/18/safari-reader.js
+
+How to print as booklet: https://statusq.org/archives/2019/01/11/8893/
+
+useful notes on makefiles and pandoc: https://zine.tildeverse.org/issue-2.html
--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+import re
 
 from courlan import clean_url
 from trafilatura import fetch_url, extract
@@ -61,18 +62,46 @@ def extract_url_contents(fetched_urls: list[FetchedURL]) -> list[ExtractedURL]:
 	extracted_urls: list[ExtractedURL] = []
 
 	for url in fetched_urls:
+		cleaned_contents = remove_emojis(url.contents)
 		extracted = extract(
-			url.contents,
+			cleaned_contents,
 			output_format="xml",
 			url=url.url,
 			include_formatting=True,
 			include_images=True,
 			include_tables=True,
 		)
-		url.contents = extracted
 		extracted_urls.append(ExtractedURL(url.url, extracted))
 
 	return extracted_urls
+
+
+# This is a temporary workaround until lxml is updated / trafilatura uses an updated lxml that fixes this issue
+def remove_emojis(text):
+	emoji_pattern = re.compile(
+		"["
+		u"\U0001F600-\U0001F64F"  # emoticons
+		u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+		u"\U0001F680-\U0001F6FF"  # transport & map symbols
+		u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+		u"\U00002500-\U00002BEF"  # chinese char
+		u"\U00002702-\U000027B0"
+		u"\U00002702-\U000027B0"
+		u"\U000024C2-\U0001F251"
+		u"\U0001f926-\U0001f937"
+		u"\U00010000-\U0010ffff"
+		u"\u2640-\u2642"
+		u"\u2600-\u2B55"
+		u"\u200d"
+		u"\u23cf"
+		u"\u23e9"
+		u"\u231a"
+		u"\ufe0f"  # dingbats
+		u"\u3030"
+		"]+",
+		flags=re.UNICODE
+	)
+	return emoji_pattern.sub(r'', text)
 
 
 if __name__ == "__main__":
